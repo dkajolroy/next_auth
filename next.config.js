e496ace7502d87173096b801045f8880ba7aa73c
kajolroy <shrikajol@gmail.com>
2023-04-03 23:49:31 +0600

    refresh

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: [],
-  },
-  env: {
-    MONGODB_URI: "mongodb://127.0.0.1:27017/next-auth",
-  },
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- utils/serverInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface RegisterBodyReq {
-  name?: string;
-  email?: string;
-  password?: string;
-  address?: string;
-}
