dc37bde1aeef148a2b210d90bbc6abdd588b5dae
kajolroy <shrikajol@gmail.com>
2023-04-11 11:23:04 +0600

    fix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,26 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: String(process.env.GOOGLE_ID),
       clientSecret: String(process.env.GOOGLE_SECRET),
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
     }),
   ],
   secret: String(process.env.SECRET_KEY),
